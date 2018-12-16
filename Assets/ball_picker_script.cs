663e3b8cfbc028b76ba086c04ddb98d926847881
Kumar Kunal <kumarkunal@ufl.edu>
2018-12-16 16:25:41 -0500

    working code

---
+++ Assets/ball_picker_script.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ball_picker_script : MonoBehaviour {
+
+    public Transform ball;
+    public Transform player;
+    private Transform arm1;
+    private Transform arm2;
+    public float lerpSpeed;
+    public bool picked = false;
+
+	void Start () {
+        player = GameObject.Find("Player").transform;
+        arm1 = GameObject.Find("arm1").transform;
+        arm2 = GameObject.Find("arm2").transform;
+        lerpSpeed = 5f;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if ((other.tag == "Player") && picked == false)
+        {
+            ball.parent = GameObject.Find("Player").transform;
+            ball.GetComponent<Rigidbody>().useGravity = false;
+            ball.GetComponent<Rigidbody>().isKinematic = true;
+            lerp();
+            picked = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        print("out of the pickup zone");
+    }
+
+    public Vector3 getHoldingPosition()
+    {
+
+        float xcoord = ((arm1.position.x + arm2.position.x) / 2 + 0.25f);
+        float ycoord = (arm1.position.y + arm2.position.y) / 2;
+        float zcoord = (arm1.position.z + arm2.position.z) / 2;
+        Vector3 pos = new Vector3(xcoord, ycoord, zcoord);
+        return pos;
+    }
+    
+    // Could use both both 1.lerp  2. directly equate coordinates.
+    public void lerp()
+    {
+        ball.position = Vector3.Lerp(ball.position, getHoldingPosition(), Time.deltaTime * lerpSpeed);
+        //ball.position = getHoldingPosition();
+    }
+}
---
+++ Assets/ball_thrower_script.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ball_thrower_script : MonoBehaviour {
+
+    public GameObject ball;
+    public GameObject pad;
+    bool should_throw = false;
+    float lerpSpeed = 50f;
+
+    //could be done via assigning to each trigger in the GUI.
+    void Start ()
+    {
+        pad = GameObject.Find(findPadTag() + "_pad");
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        print("tag check pending");
+        print(other.tag.ToString());
+        if (other.tag == ball.tag)
+        {
+            should_throw = true;
+            if (pad)
+            {
+                ball.transform.parent = null;
+                ball.GetComponent<Rigidbody>().isKinematic = true;
+                ball.GetComponent<Rigidbody>().useGravity = true;
+                print("putting the ball onto the pad now!!");
+                ball.transform.position = Vector3.Lerp(ball.transform.position, pad.transform.position, Time.deltaTime * lerpSpeed);
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        print("out of throw zone:");
+    }
+
+    private string findPadTag()
+    {
+        string pad_tag = ball.tag.ToString();
+        pad_tag = pad_tag.Substring(0, pad_tag.IndexOf("_"));
+        print(pad_tag);
+        return pad_tag;
+    }
+}
